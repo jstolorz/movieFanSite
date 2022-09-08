b9bfe9e8013b68dfb3454e730ac5aa6abf02d0d6
Janusz <jstolorz@gmail.com>
2022-09-08 16:07:27 +0200

    adding mysql db connection

---
+++ databases/mysqldb.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql')
+
+const pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'bob',
+    password        : 'secret',
+    database        : 'my_db'
+});
+
+pool.query('select * from test', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+})
