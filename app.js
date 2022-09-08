12cbdf63d24e046581aa3d488ef7592d113f28b8
Janusz <jstolorz@gmail.com>
2022-09-08 13:21:17 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const passportConfig = require('./config')
+const passport = require('passport')
+const GitHubStrategy = require('passport-github')
+const helmet = require('helmet')
+const session = require('express-session')
+
+var indexRouter = require('./routes/index');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// app.use(helmet({
+//   contentSecurityPolicy: false,
+//   crossOriginEmbedderPolicy: false,
+// }))
+
+
+app.use(session({
+  secret: 'tymek'
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new GitHubStrategy(passportConfig, (accessToken, refreshToken, profile, cb) => {
+  return cb(null, profile);
+}))
+
+passport.serializeUser((user, cb)=>{
+  cb(null,user);
+})
+passport.deserializeUser((user,cb)=>{
+  cb(null,user)
+})
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport')
+const request = require('request')
+const {use} = require("express/lib/router");
+const {response} = require("express");
+
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8'
+//const apiKey = '123456789'
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+//const apiBaseUrl = 'http://localhost:3030'
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+router.use((req,res,next) => {
+    res.locals.imageBaseUrl = imageBaseUrl
+    next()
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  request.get(nowPlayingUrl,(error,response,moveData) => {
+     const parseData = JSON.parse(moveData)
+    res.render('index', {
+       parsedData: parseData.results
+    });
+  })
+});
+
+router.get('/login',passport.authenticate('github'))
+
+router.get('/auth',passport.authenticate('github',{
+    successRedirect: '/',
+    failureRedirect: 'loginFailed'
+}))
+
+router.get('/favorites',(req,res) =>{
+    res.json(req.user.displayName)
+})
+
+
+router.get('/movie/:id',(req, res, next)=>{
+    // res.json(req.params.id);
+    const movieId = req.params.id;
+    const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`
+    // res.send(thisMovieUrl)
+    request.get(thisMovieUrl,(error, response, movieData)=>{
+        console.log(typeof(movieData))
+        const parsedData = JSON.parse(movieData)
+        res.render('single-movie',{
+            parsedData
+        })
+    })
+})
+
+router.post('/search',(req,res,next) => {
+    const userSearchTerm = encodeURI(req.body.movieSearch)
+    const cat = req.body.cat
+    const movieUrl = `${apiBaseUrl}/search/${cat}?query=${userSearchTerm}&api_key=${apiKey}`
+    request.get(movieUrl,(error,response, movieData) => {
+        let parsedData = JSON.parse(movieData)
+
+        if(cat === 'person'){
+            parsedData.results = parsedData.results[0].known_for
+        }
+
+        res.render('index',{
+            parsedData: parsedData.results
+        })
+    })
+})
+
+
+module.exports = router;
